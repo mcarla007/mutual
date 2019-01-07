52573186a2eb3f89e9470910ae039987bb1536e8
mcarla007 <mcarla007@outlook.com>
2019-01-07 15:22:53 -0300

    mi primer commit

---
+++ MVC/index.php
@@ -0,0 +1,24 @@
+<?php
+
+  $var1 = 23;
+  $nombre = "Carla";
+
+  echo "Mi nombre es $nombre"."<br>";
+
+ //$edad = array('adulto' => , );
+ $autos = array("camioneta", "jeep", "autochico");
+
+$edad = array('0' =>23 , '1' =>12);
+
+ print_r ($autos);
+ print_r ("<h1>$edad[1]</h1>");
+
+ $hora = 15;
+
+ if ($hora > 6 and  $hora < 12)
+    echo "<h1>Hola buenos días</h1>";
+ else {
+   echo "<h1>Hola buenas tardes</h1>";
+ }
+
+ ?>
---
+++ MVC/views/template.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <header>
+    <h1>"logotipo"</h1>
+  </header>
+
+  <nav>
+    <ul>
+      <li> <a href=""> Inicio</a> </li>
+      <li> <a href=""> Quienes Somos </a></li>
+      <li> <a href=""> Servicios </a></li>
+      <li> <a href=""> Contactanos </a></li>
+    </ul>
+  </nav>
+
+  <section>
+    <h1>Página de Inicio</h1>
+  </section>
+
+  </body>
+
+
+</html>
